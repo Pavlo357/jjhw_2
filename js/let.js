391ced56bdeed7741044956ff8aa8b02f51bc4bd
Pavlo357 <borealis579@gmail.com>
2021-04-26 15:38:19 +0300

    homework

---
+++ js/let.js
@@ -0,0 +1,24 @@
+//Task # 1
+
+
+let name = "Джон";
+let admin;
+admin = name;
+alert(admin);
+
+//Task # 2
+
+
+var Our_Planet_Name = "Earth";
+
+var Current_Visitor_Name = "John";
+
+
+
+//Task # 3
+
+let example_number = 10;
+let example_string = "10";
+
+console.log(typeof example_number, typeof example_string);
+console.log(example_number + example_string)
